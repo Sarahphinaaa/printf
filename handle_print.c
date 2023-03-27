ab4b0aa8bb86ac90b7460561e4d6eaf0aaf68a3e
Boitumelo <boitumeloditire@ymail.com>
2023-03-27 04:41:20 -0700

    Updated

--- handle_print.c
+++ handle_print.c
@@ -2,7 +2,7 @@
 
 /**
 * handle_print - prints an argument based on its type
-* @format: the formatted string used to print the arguments
+* @fmt: the formatted string used to print the arguments
 * @list: list of arguments
 * @ind: ind
 * @buffer: Buffer array to handle print
@@ -12,46 +12,46 @@
 * @size: size specifier
 * Return: 1 or 2
 */
-int handle_print(const char *format, int *ind, va_list list, char buffer[],
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknown_len = 0, printed_chars = -1;
-	format_t format_types[] = {
+	fmt_t fmt_types[] = {
 	{'c', print_char}, {'s', print_string}, {'%', print_percentg},
 	{'i', print_int}, {'d', print_int}, {'b', print_binary},
 	{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 	{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 	{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	}
-	for (i = 0; format_types[i].format != '\0'; i++)
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 	{
-		if (format[*ind] == format_types[i].format)
+		if (fmt[*ind] == fmt_types[i].fmt)
 		{
-			return (format_types[i].format(list, buffer, flags,
+			return (fmt_types[i].fn(list, buffer, flags,
 						width, precision, size));
 		}
 	}
-	if (format_types[i].format == '\0')
+	if (fmt_types[i].fmt == '\0')
 	{
-		if (format[*ind] == '\0')
+		if (fmt[*ind] == '\0')
 		{
 			return (-1);
 		}
 		unknown_len += write(1, "%%", 1);
-		if (format[*ind - 1] == ' ')
+		if (fmt[*ind - 1] == ' ')
 		{
 			unknown_len += write(1, " ", 1);
 		}
 		else if (width)
 		{
 			--(*ind);
-			while (format[*ind] != ' ' && format[*ind] != '%')
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
-			if (format[*ind] == ' ')
+			if (fmt[*ind] == ' ')
 				--(*ind);
 			return (1);
 		}
-		unknown_len += write(1, &format[*ind], 1);
+		unknown_len += write(1, &fmt[*ind], 1);
 		return (unknown_len);
 	}
 	return (printed_chars);
